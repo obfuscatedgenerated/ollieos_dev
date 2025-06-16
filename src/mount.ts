c7f0688914debb6c9f89f71f0ab2df11f616e289
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-06-16 18:29:33 +0100

    mount program

---
+++ src/mount.ts
@@ -0,0 +1,39 @@
+import type { Program } from "ollieos/src/types";
+import {ANSI} from "ollieos/src/term_ctl";
+import {mount_and_register_with_output} from "ollieos/src/prog_registry";
+
+export default {
+    name: "mount",
+    description: "Mounts OllieOS programs in the virtual filesystem as programs manually.",
+    usage_suffix: "<path>",
+    arg_descriptions: {
+        "path": "The path to the program to mount."
+    },
+    main: async (data) => {
+        // extract from data to make code less verbose
+        const { term, args } = data;
+
+        const { STYLE, PREFABS, FG } = ANSI;
+
+        // check if path is provided
+        if (args.length === 0) {
+            term.writeln(`${PREFABS.error}A single argument, the path, is required.${STYLE.reset_all}`);
+            return 1;
+        }
+
+        const fs = term.get_fs();
+        const path = args[0];
+
+        if (!fs.exists(path)) {
+            term.writeln(`${PREFABS.error}Path '${path}' does not exist.${STYLE.reset_all}`);
+            return 1;
+        }
+
+        const content = fs.read_file(path) as string;
+
+        const registry = term.get_program_registry();
+        mount_and_register_with_output(path, content, registry, term, true);
+
+        return 0;
+    }
+} as Program;
